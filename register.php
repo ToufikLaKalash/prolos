15216e7441ac2d8432728675774563ccbfc46d70
TotorDuMordor <43754543+TotorDuMordor@users.noreply.github.com>
2018-12-11 15:57:25 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+$titre="Enregistrement";
+include("includes/identifiants.php");
+include("includes/debut.php");
+include("includes/menu.php");
+echo '<p><i>Vous êtes ici</i> : <a href="./index.php">Index du forum</a> --> Enregistrement';
+
+if ($id!=0) erreur(ERR_IS_CO);
+	
+	else //On est dans le cas traitement
+{
+    $pseudo_erreur1 = NULL;
+    $pseudo_erreur2 = NULL;
+    $mdp_erreur = NULL;
+    $email_erreur1 = NULL;
+    $email_erreur2 = NULL;
+    $msn_erreur = NULL;
+    $signature_erreur = NULL;
+    $avatar_erreur = NULL;
+    $avatar_erreur1 = NULL;
+    $avatar_erreur2 = NULL;
+    $avatar_erreur3 = NULL;
+} //Fin de la partie formulaire
+    //On récupère les variables
+    $i = 0;
+    $temps = time(); 
+    $pseudo=$_POST['pseudo'];
+    $signature = $_POST['signature'];
+    $email = $_POST['email'];
+    $msn = $_POST['msn'];
+    $website = $_POST['website'];
+    $localisation = $_POST['localisation'];
+    $pass = md5($_POST['password']);
+    $confirm = md5($_POST['confirm']);
+	
+    //Vérification du pseudo
+    $query=$db->prepare('SELECT COUNT(*) AS nbr FROM forum_membres WHERE membre_pseudo =:pseudo');
+    $query->bindValue(':pseudo',$pseudo, PDO::PARAM_STR);
+    $query->execute();
+    $pseudo_free=($query->fetchColumn()==0)?1:0;
+    $query->CloseCursor();
+    if(!$pseudo_free)
+    {
+        $pseudo_erreur1 = "Votre pseudo est déjà utilisé par un membre";
+        $i++;
+    }
+
+    if (strlen($pseudo) < 3 || strlen($pseudo) > 15)
+    {
+        $pseudo_erreur2 = "Votre pseudo est soit trop grand, soit trop petit";
+        $i++;
+    }
+
+    //Vérification du mdp
+    if ($pass != $confirm || empty($confirm) || empty($pass))
+    {
+        $mdp_erreur = "Votre mot de passe et votre confirmation diffèrent, ou sont vides";
+        $i++;
+    }
+
+
+    //On récupère les variables
+    $i = 0;
+    $temps = time(); 
+    $pseudo=$_POST['pseudo'];
+    $signature = $_POST['signature'];
+    $email = $_POST['email'];
+    $msn = $_POST['msn'];
+    $website = $_POST['website'];
+    $localisation = $_POST['localisation'];
+    $pass = md5($_POST['password']);
+    $confirm = md5($_POST['confirm']);
+	
+    //Vérification du pseudo
+    $query=$db->prepare('SELECT COUNT(*) AS nbr FROM forum_membres WHERE membre_pseudo =:pseudo');
+    $query->bindValue(':pseudo',$pseudo, PDO::PARAM_STR);
+    $query->execute();
+    $pseudo_free=($query->fetchColumn()==0)?1:0;
+    $query->CloseCursor();
+    if(!$pseudo_free)
+    {
+        $pseudo_erreur1 = "Votre pseudo est déjà utilisé par un membre";
+        $i++;
+    }
+
+    if (strlen($pseudo) < 3 || strlen($pseudo) > 15)
+    {
+        $pseudo_erreur2 = "Votre pseudo est soit trop grand, soit trop petit";
+        $i++;
+    }
+
+    //Vérification du mdp
+    if ($pass != $confirm || empty($confirm) || empty($pass))
+    {
+        $mdp_erreur = "Votre mot de passe et votre confirmation diffèrent, ou sont vides";
+        $i++;
+    }
+?>
+<?php
+$query=$db->prepare('SELECT COUNT(*) AS nbr FROM forum_membres WHERE membre_pseudo =:pseudo');
+$query->bindValue(':pseudo',$pseudo, PDO::PARAM_STR);
+$query->execute();
+$pseudo_free=($query->fetchColumn()==0)?1:0;
+?>
+<?php
+    //Vérification de l'adresse email
+
+    //Il faut que l'adresse email n'ait jamais été utilisée
+    $query=$db->prepare('SELECT COUNT(*) AS nbr FROM forum_membres WHERE membre_email =:mail');
+    $query->bindValue(':mail',$email, PDO::PARAM_STR);
+    $query->execute();
+    $mail_free=($query->fetchColumn()==0)?1:0;
+    $query->CloseCursor();
+    
+    if(!$mail_free)
+    {
+        $email_erreur1 = "Votre adresse email est déjà utilisée par un membre";
+        $i++;
+    }
+    //On vérifie la forme maintenant
+    if (!preg_match("#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#", $email) || empty($email))
+    {
+        $email_erreur2 = "Votre adresse E-Mail n'a pas un format valide";
+        $i++;
+    }
+    }
+    //Vérification de la signature
+    if (strlen($signature) > 200)
+    {
+        $signature_erreur = "Votre signature est trop longue";
+        $i++;
+    }
+?>
+
+<?php
+//Message
+$message = "Bienvenue sur mon super forum !";
+//Titre
+$titre = "Inscription à mon super forum !";
+
+mail($_POST['email'], $titre, $message);
+?>
